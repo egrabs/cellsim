1b3cce3e745b868f0aad2db7da389ad0e04a46f1
Elyes Graba <elyesgraba@gatech.edu>
2016-08-29 15:57:12 -0400

    Initial setup of data version for parameter sweep

--- ellipSim.py
+++ ellipSim.py
@@ -3,6 +3,7 @@ from math import *
 from cell import *
 from cellHelper import *
 import xlsxwriter as xls
+import sys
 
 __author__ = "Elyes Graba"
 __credits__ = ["Peter Yunker", "Shane Jacobeen"]
@@ -17,7 +18,7 @@ cellList = [] #a list of all the cell objects that currently exist
 #NOTE unused currently, no need for reproduction probablility
 doublingProb = 0.50 #for now we use a hard-coded doubling probability of 95%
 
-workbook = xls.Workbook("CumulativeOverlapvsGenerationWithAspect15.xlsx")
+workbook = xls.Workbook(sys.argv[1])
 worksheet = workbook.add_worksheet()
 
 #arrows meant to graphically represent x,y, and z axes
@@ -59,89 +60,91 @@ for q in range(10):
 
 row += 1
 
+overlap_params = [0.15, 0.30, 0.50, 0.70, 0.85, 0.95]
+
 aspRat = 1.5
 
 #HACK get rid of the aspect ratio loop and instead loop over the trial number
 
 #for aspRat in aspectList: #loop over the aspect ratios, to perform a trial at each aspect ratio.
 
-for trialNumber in range(10):
+for overlap_parameter in overlap_params:
 
-    overlaps = 0 #to count the number of overlap cases
+    worksheet.write(row, col+12, 'Overlap Parameter: ')
+    worksheet.write(row, col+13, overlap_parameter)
 
+    for trialNumber in range(10):
 
-    rootCell = RootCell(vector(0,0,0), aspRat*diamArray[0], diamArray[0], vector(1, 2, 3), None, 0) #The original cell, note that it has generation = 0
-    cellList.append(rootCell) #put the root cell in the cellList
+        overlaps = 0 #to count the number of overlap cases
+        rootCell = RootCell(vector(0,0,0), aspRat*diamArray[0], diamArray[0], vector(1, 2, 3), None, 0) #The original cell, note that it has generation = 0
+        cellList.append(rootCell) #put the root cell in the cellList
 
+        for i in iterationList: #iterate over the number of gens
 
+            if trialNumber == 0: #only need to write the generation numbers on the first run-through
+                worksheet.write(row,col,i)
 
-    for i in iterationList: #iterate over the number of gens
+            cumulativeOverlap = sum([sum(zell.overlaps) for zell in cellList])
 
-        if trialNumber == 0: #only need to write the generation numbers on the first run-through
-            worksheet.write(row,col,i)
+            worksheet.write(row, col+1, cumulativeOverlap)
 
-        cumulativeOverlap = sum([sum(zell.overlaps) for zell in cellList])
+            row += 1
+            # if i != 1:
+            #     currText.visible = False
+            # currText = text(text='Generation\n' + str(i), align='center', height=10, width=10,pos=vector(50,20,20),depth=-0.3, color=color.green)
+            length = aspRat*diamArray[i - 1] #follows from aspRat = length / diam
+            temp = []  # holds the newly created cells until the current round of reproduction is over
 
-        worksheet.write(row, col+1, cumulativeOverlap)
+            # if random.random() <= 0.10:
+            #     calling this method switches the pole from which the root cell spawns
+            #     rootCell.switchGrowthDirection()
 
-        row += 1
+            for cel in cellList: #for each cell currently in the cellList
 
-        #if i != 1:
-            #currText.visible = False
-        #currText = text(text='Generation\n' + str(i), align='center', height=10, width=10,pos=vector(50,20,20),depth=-0.3, color=color.green)
-        length = aspRat*diamArray[i - 1] #follows from aspRat = length / diam
-        temp = [] #holds the newly created cells until the current round of reproduction is over
+                #rate(10) #for visual purposes, slow the rate down to see the network grow in real time
 
-        #if random.random() <= 0.10:
-            #calling this method switches the pole from which the root cell spawns
-            #rootCell.switchGrowthDirection()
+                #if random.uniform(0, 1) < doublingProb: #if a randomly generated float in [0,1] is less than the doubling probability, then double
 
-        for cel in cellList: #for each cell currently in the cellList
+                variedTheta = computeVariedTheta(theta, thetaVariance) #generate a slight random variance in theta to reduce unintended patterns
+                (cellPos, direc) = getDaughterPos(cel, variedTheta, length)
 
-            #rate(10) #for visual purposes, slow the rate down to see the network grow in real time
+                #colTemp = cel.graphical.color
+                #cel.graphical.color=color.magenta
+                #wait(1)
 
-            #if random.uniform(0, 1) < doublingProb: #if a randomly generated float in [0,1] is less than the doubling probability, then double
+                if (cellPos,direc) != (None, None):
 
+                    newCell = Cell(cellPos, length, diamArray[i-1], direc, cel, i) #create the daughter
 
-
-
-            variedTheta = computeVariedTheta(theta, thetaVariance) #generate a slight random variance in theta to reduce unintended patterns
-            (cellPos, direc) = getDaughterPos(cel, variedTheta, length)
-
-            #colTemp = cel.graphical.color
-            #cel.graphical.color=color.magenta
-            #wait(1)
-
-            if (cellPos,direc) != (None, None):
-
-                newCell = Cell(cellPos, length, diamArray[i-1], direc, cel, i) #create the daughter
-
-                ovFail = checkOverlap(newCell, cellList, temp, overlapParam) #check if the daughter overlaps with any existing cells
+                    ovFail = checkOverlap(newCell, cellList, temp, overlapParam) #check if the daughter overlaps with any existing cells
 
 
 
-                if (not ovFail): #if there is NOT an overlap above the chosen threshold, allow the daughter to exist
-                    cel.children.append(newCell) #add it to the children list of its parent
-                    temp.append(newCell) #add it to the temporary storage list
+                    if (not ovFail): #if there is NOT an overlap above the chosen threshold, allow the daughter to exist
+                        cel.children.append(newCell) #add it to the children list of its parent
+                        temp.append(newCell) #add it to the temporary storage list
 
-                else:
-                    cel.failedSpawns += 1
-                    #newCell.graphical.visible = False #make the daughter disappear if it overlapped too much with an existing cell
-                    #for sphr in newCell.sphereMesh:
-                        #sphr.graphical.visible = False
+                    else:
+                        cel.failedSpawns += 1
+                        #newCell.graphical.visible = False #make the daughter disappear if it overlapped too much with an existing cell
+                        #for sphr in newCell.sphereMesh:
+                            #sphr.graphical.visible = False
 
-        for cel in temp: #once the reproduction cycle is complete,
+            for cel in temp: #once the reproduction cycle is complete,
 
-            cellList.append(cel) #add all the newly created daughters in temp to the main cellList
+                cellList.append(cel) #add all the newly created daughters in temp to the main cellList
 
-    #exportAsOpenSCAD(cellList, aspectList[0])
+        #exportAsOpenSCAD(cellList, aspectList[0])
 
-    temp = []
-    cellList = [] #if doing multiple trials, we need to reset these arrays before starting the next trial
-    col += 1 #move the excel coordinate one column over for the next trial
-    row = 1 #row coordinate goes back to just below the columnt titles
+        temp = []
+        cellList = [] #if doing multiple trials, we need to reset these arrays before starting the next trial
+        col += 1 #move the excel coordinate one column over for the next trial
+        row = 1 #row coordinate goes back to just below the columnt titles
+
+    row += numGens + 2
+    col = 0
 
 workbook.close()
 
-exit() #if this is uncommented the program will exit when it is finished running, you wont have time to view the visual representation of the cluster
+sys.exit() #if this is uncommented the program will exit when it is finished running, you wont have time to view the visual representation of the cluster
 #it's mostly only used when collecting data and not using the visual mode
